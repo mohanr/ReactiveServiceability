41bafbd26b159f7bae4b359d99ed415dd429ef8d
mohanr <radhakrishnan.mohan@gmail.com>
2014-06-18 13:00:37 +0530

    Initial commit

--- R/server.R
+++ R/server.R
@@ -1,9 +1,6 @@
 library(shiny)
 library(rJava)
-setwd(path.expand("~/") )
 .jinit(parameters="-Xmx512m")
 shinyServer(function(input, output) {
 print(.jclassPath())
-younggenprobe <- .jnew("com/memory/probe/YoungGenProbe")
-print(.jcall(younggenprobe,"J","getYoungCapacity"))
 })
